7035bd05c6f179bea2fa36510f159f83173df606
Connor <connordbrown@gmail.com>
2023-07-18 21:15:37 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const { describe } = require("mocha");
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(wrapper = '*') {
+    return function (descr = 'special') {
+        return `You are ${wrapper}${descr}${wrapper}!`;
+    }
+}
